d1c3e63fd006ae3bc3005fced2d054051449e1c4
BugsBunny421 <rainbowmirza@gmail.com>
2021-09-02 00:00:44 +0500

    Create Macros.cpp

---
+++ Exam/Macros.cpp
@@ -0,0 +1,11 @@
+//The preprocessor performs preliminary operations on C and C++ files before they are passed to the compiler
+
+#include <iostream> //Include Preprocessor
+
+//#define directive allows the definition of macros within your source code
+#define WIDTH  80
+
+using namespace std;
+int main(){
+	cout << WIDTH; //Already Defined
+}
